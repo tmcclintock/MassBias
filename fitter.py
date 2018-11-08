aea18fcd7c301dc6655cc04542ac9c66ed38ac52
Tom McClintock <tmcclintock89@gmail.com>
2018-11-08 16:52:18 -0500

    Making the fitter. Remade all the data correctly this time.

--- fitter.py
+++ fitter.py
@@ -5,6 +5,19 @@ import numpy as np
 import cluster_toolkit as ct
 
 
+def lnlike(pars, args):
+    Mtrue = args['Mass']
+    Cal = pars
+    M = Mtrue/Cal
+    DSmodel = get_model(M, args)[args['inds']]
+    #Get the data
+    DSd = args['DSd']
+    icov = args['icov']
+    X = DSd - DSmodel
+    chi2 = -0.5*np.dot(X,np.dot(icov,X))
+    return chi2
+
+
 if __name__ == "__main__":
     #Load in the halo catalog
     #data = np.load("testdata/reduced_halos_lamobs_0.20sigintr_009.npy")
@@ -13,7 +26,7 @@ if __name__ == "__main__":
     bins = np.array([20,30,45,60,999])
     zs = [1.0, 0.5, 0.25, 0.0]    
 
-    covpath = ""
+    covpath = "/Users/tmcclintock/Data/DATA_FILES/y1_data_files/FINAL_FILES/SACs/SAC_z%d_l%d.txt"
     datapath = ""
 
     for sig in sigs:
@@ -34,3 +47,9 @@ if __name__ == "__main__":
             k = np.loadtxt("testdata/k.txt")
             Plin = np.loadtxt("testdata/plin_z%d.txt"%(ind-6))
             Pnl = np.loadtxt("testdata/pnl_z%d.txt"%(ind-6))
+
+            #Distances
+            r = np.logspace(-3, 3, 1000) #Mpc/h comoving
+            Rperp = np.logspace(-3, 2.4, 1000) #Mpc/h comoving
+
+            
